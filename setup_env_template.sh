33da5e8b50e47c3f981c7bedf08d18024f7fe789
Florin CELOIU <celoiuflorinelvis@yahoo.co.uk>
2026-04-08 06:18:21 +0300

    chore: add setup_env_template.sh with placeholder env vars

---
+++ setup_env_template.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export GITHUB_CLIENT_ID="<your-github-client-id>"
+export GITHUB_CLIENT_SECRET="<your-github-client-secret>"
+export API_BASE="http://localhost:8080"
+export REDIRECT_URI="http://localhost:8080"
